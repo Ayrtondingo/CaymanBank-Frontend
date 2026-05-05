aaea7d53d4af51a8850db376950b1e8d70ca7cc9
Ayrtondingo <ayrton_d_@hotmail.com>
2026-05-05 12:46:38 -0300

    fix: change next config to commonjs format

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Ignora los errores de tipo en el build de Vercel
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignora los errores de linting en el build de Vercel
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
